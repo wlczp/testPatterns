276e4ff1350a9b16060094a67e66233793961bb0
Administrator <bodesc@163.com>
2019-11-20 22:45:44 +0800

    修改Test

--- src/com/wlczp/designpatterns/factory/Test.java
+++ src/com/wlczp/designpatterns/factory/Test.java
@@ -1,6 +1,6 @@
 package com.wlczp.designpatterns.factory;
 
-import edu.princeton.cs.algs4.*;
+//import edu.princeton.cs.algs4.*;
 
 /**
  * 测试 设计模式之工厂模式方法
@@ -10,10 +10,12 @@ public class Test {
        PriceFactory pf = new MyPriceFactory();
        //价格对象通过工厂得到。
         Price price=pf.createPrice(Constant.OTHER);
-        StdOut.println( price.toString());
+      //  StdOut.println( price.toString());
+        System.out.println(price.toString());
         price=pf.createPrice(Constant.FORCURE);
-        StdOut.println( price.toString());
+       // StdOut.println( price.toString());
+        System.out.println(price.toString());
         price=pf.createPrice(Constant.ENTITY);
-        StdOut.println( price.toString() );
+       // StdOut.println( price.toString() );
     }
 }
